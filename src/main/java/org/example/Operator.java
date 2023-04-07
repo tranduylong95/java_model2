170d4de8082f1646a0d50caa49aef093b4d4f34d
tranduylong95 <tranduylong95@gmail.com>
2023-04-07 11:25:46 +0700

    value

--- src/main/java/org/example/Operator.java
+++ src/main/java/org/example/Operator.java
@@ -1,7 +1,20 @@
 package org.example;
-
+import java.util.Scanner;
 public class Operator {
+
     public static void main(String[] args) {
-        
+        float width;
+        float height;
+
+        Scanner scanner = new Scanner(System.in);//Khai báo đối tượng Scanner
+
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();//Nhập chiều rộng
+
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();//Nhập chiều dài
+        float area = width * height;
+
+        System.out.println("Area is: " + area);
     }
 }
